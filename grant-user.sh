0523fc67eae17a223d875542cf48615e7ca73174
Михаил Те <tpl20000@yandex.ru>
2025-06-02 02:33:05 +0700

    Fix script more

--- grant-user.sh
+++ grant-user.sh
@@ -2,4 +2,5 @@
 
 PGPASSWORD=postgres
 
-psql -h localhost -U postgres -d store -c "CREATE USER store_user WITH PASSWORD storeuserpassword; GRANT ALL PRIVILEGES ON DATABASE store to store_user;"
+psql -h localhost -U postgres -d store -c "CREATE USER store_user WITH PASSWORD storeuserpassword"
+psql -h localhost -U postgres -d store -c "GRANT ALL PRIVILEGES ON DATABASE store to store_user"
