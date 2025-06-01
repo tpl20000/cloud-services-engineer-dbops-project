0c3f0eedcf0388fdf0bb58091e48e1a22d1b1a0b
Михаил Те <tpl20000@yandex.ru>
2025-06-02 02:37:06 +0700

    Remove the script

--- grant-user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PGPASSWORD=postgres
-
-psql -h localhost -U postgres -d store -c "CREATE USER store_user WITH PASSWORD storeuserpassword"
-psql -h localhost -U postgres -d store -c "GRANT ALL PRIVILEGES ON DATABASE store to store_user"
